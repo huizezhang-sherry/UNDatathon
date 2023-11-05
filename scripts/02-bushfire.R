eca68e82e78ed1f4d43a1d55006206fe54d3e90b
huizezhang-sherry <huizezhangsh@gmail.com>
2023-11-05 10:26:40 +1000

    bushfire EDA

--- scripts/02-bushfire.R
+++ scripts/02-bushfire.R
@@ -31,12 +31,38 @@ bushfire3 |>
           size= 0.7, fill = "grey", color = "grey90") +
   geom_sf(fill = "orange") +
   geom_sf(data=  bushfire_big2, fill = "red") +
-  # geom_sf(data = bioregion_koala |> sf::st_cast("MULTILINESTRING"), #fill = "grey",
-  #         color = "black", size = 0.7) +
+  geom_sf(data = bioregion_koala |> sf::st_cast("MULTILINESTRING"), #fill = "grey",
+           color = "black", size = 0.7) +
   coord_sf(xlim = c(150, 155), ylim = c(-33, -26)) +
   theme_bw()
 
-dt <- koala |>
-  filter(LAT < 0) |>
-  sf::st_as_sf(coords = c("LON", "LAT"), crs = 4326) |>
-  st_filter(bushfire_big2)
+koala_dt <- koala |>
+  st_filter(bushfire_big2) |>
+  dplyr::mutate(long = st_coordinates(geometry)[,1],
+                lat =  st_coordinates(geometry)[,2]) |>
+  as_tibble() |>
+  mutate(region = ifelse(lat < - 29.5, "liberation_trail", "Myall Creek Road"))
+
+koala_dt |> filter(year > 2015) |>
+  ggplot() +
+  geom_sf(data = bushfire_big2, fill = "red") +
+  geom_point(aes(x = long, y = lat)) +
+  facet_wrap(~year, nrow = 2)
+
+library(tsibble)
+kl_count <- koala_dt |>
+  filter(between(year, 2010, 2022)) |>
+  count(region, year, month)
+
+kl_count |>
+  mutate(ym = tsibble::make_yearmonth(year = year, month = month)) |>
+  ggplot(aes(x = ym, y = n, group = region)) +
+  geom_line() +
+  scale_x_yearmonth(date_labels = "%y %b", date_breaks = "6 month")
+
+kl_count |>
+  ggplot(aes(x = as.factor(month),  y = n, color = region,
+             group = interaction(as.factor(year), region))) +
+  geom_line() +
+  facet_wrap(~year, ncol = 3)
+
